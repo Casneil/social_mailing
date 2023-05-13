974851325fb2c91dbe583092d2bea2b4e0ab9af0
Casneil <casneil.simpson@gmail.com>
2023-05-13 22:47:47 +0200

    setting up swagger

--- src/swagger/options.ts
+++ src/swagger/options.ts
@@ -1,3 +1,14 @@
 export const options = {
+<<<<<<< HEAD
+=======
+	definition: {
+		info: {
+			title: 'OpenAPI Tweets endpoints',
+			description: 'OpenAPI specification For tweets api',
+			version: '0.0.1'
+		},
+		servers: [{ url: `http://localhost:${ PORT }` }]
+	},
+>>>>>>> e279e55 (setting up swagger)
 	apis: ['./src/routes/*.ts']
 };
