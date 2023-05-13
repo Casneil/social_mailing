4ae29fcf2e4adb5275c1393473f12778a8b69412
Casneil <casneil.simpson@gmail.com>
2023-05-13 23:13:56 +0200

    remove jsdoc package and update readme

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import userRoutes from './routes/userRoutes';
 import tweetRoutes from './routes/tweetRoutes';
 import authRoute from './routes/authRoute';
 import { authenticateUser } from './middlewares/auth';
-import { specs } from './swagger/index';
-import { PORT } from './utils/getServerPort';
 import { docs } from './swagger/docs';
+import { PORT } from './utils/getServerPort';
 
 const app = express();
 
@@ -14,7 +13,7 @@ app.use(express.json());
 app.use('/api/user', authenticateUser, userRoutes);
 app.use('/api/tweet', authenticateUser, tweetRoutes);
 app.use('/api/auth', authRoute);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs, specs));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
 
 app.listen(PORT, () => {
 	if (process.env.NODE_ENV === 'dev') {
--- src/swagger/index.ts
+++
@@ -1,4 +0,0 @@
-import swaggerJsDoc from 'swagger-jsdoc';
-import { options } from './options';
-
-export const specs = swaggerJsDoc(options);
